a18691f229e7105a02df490ba4e7e6a5fedb566f
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2024-12-26 13:58:05 -0300

    Implementação da classe RentSpecRepository

--- src/main/java/rentnow/repository/RentSpecRepository.java
+++ src/main/java/rentnow/repository/RentSpecRepository.java
@@ -2,8 +2,23 @@ package rentnow.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import rentnow.enums.SpecType;
+import rentnow.model.Rent;
 import rentnow.model.RentSpec;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RentSpecRepository extends JpaRepository<RentSpec, Long> {
+
+   List<RentSpec> findByrentSpecId(long rentSpecId);
+
+   Optional<RentSpec> findByrent(Rent rent);
+
+   Optional<RentSpec> findByspecId(SpecType specType);
+
+   Optional<RentSpec> findByspecValue(String specValue);
+
+   void deleteBySpecId(long rentSpecId);
 }
