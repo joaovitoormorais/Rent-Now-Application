e08549df92246f62be6fc50cc77e911251177592
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2024-12-26 13:31:35 -0300

    implementação da classe RentImageRepository

--- src/main/java/rentnow/repository/RentImageRepository.java
+++ src/main/java/rentnow/repository/RentImageRepository.java
@@ -5,8 +5,16 @@ import org.springframework.stereotype.Repository;
 import rentnow.model.RentImage;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RentImageRepository extends JpaRepository<RentImage, Long> {
 
+    List<RentImage> findByRentId(Long rentId);
+
+    Optional<RentImage> findByPath(String path);
+
+    long countByRentId(Long rentId);
+
+    void deleteByRentId(Long rentId);
 }
