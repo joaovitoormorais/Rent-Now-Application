c41968d778c5a18253733b3935c6c6671c493e89
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2024-12-26 13:10:27 -0300

    Criação e implementação da classe RentRepository

--- src/main/java/rentnow/repository/RentRepository.java
+++ src/main/java/rentnow/repository/RentRepository.java
@@ -1,9 +1,12 @@
 package rentnow.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import rentnow.model.Rent;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,4 +19,10 @@ public interface RentRepository extends JpaRepository<Rent, UUID> {
 
     List<Rent> findBySummaryContainingIgnoreCase(String keyword);
 
+    List<Rent> findByCreateAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    @Query(value = "SELECT * FROM rents WHERE rent_price > :price", nativeQuery = true)
+    List<Rent> findByPriceGreatherThan(@Param("price") double price);
+
+    boolean existById(UUID uuid);
 }
